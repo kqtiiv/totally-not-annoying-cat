0f8f139d07658ce2736f1c96ab800a33bdf0d7f0
Ashlyn Chor <ahc25@ic.ac.uk>
2026-05-17 14:15:07 +0100

    added servo logic

--- main.ino
+++ main.ino
@@ -1,5 +1,6 @@
 #include <WiFi.h>
 #include <WebServer.h>
+#include <Servo.h>
 #include <secrets.cpp>
 
 // Enter your home Wi-Fi network details here
@@ -7,39 +8,38 @@ const char* ssid = SSID;
 const char* password = PASSWORD;
 
 int counter = 0;
+bool thrown = false;
 
 WebServer server(80);
 String currentStatus = "IDLE";
+Servo myServo;
+
 const int triggerPin = 1;
+int pos = 0;
 
-// NEW: This keeps track of whether we have already triggered a sound
-bool soundTriggered = false; 
+bool soundTriggered = false;
 
-// Sends the current status (IDLE or PLAY) to your iPhone app
 void handleStatus() {
     server.sendHeader("Connection", "close");
     server.send(200, "text/html", currentStatus);
 }
 
-// Resets the status back to IDLE AND clears our trigger flag
 void handleReset() {
     server.sendHeader("Connection", "close");
     currentStatus = "IDLE";
-    soundTriggered = false; // ALLOWS THE ESP32 TO TRIGGER THE NEXT SOUND
+    soundTriggered = false;
     server.send(200, "text/plain", "RESET_OK");
 }
 
 void setup() {
-    // Start serial communication at 115200 baud rate
     pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+    digitalWrite(LED_BUILTIN, HIGH);
 
     Serial.begin(115200);
     
-    // Configure the on-board button pin
     pinMode(triggerPin, INPUT_PULLUP);
+    myServo.attach(8);
 
-    // Connect to your Wi-Fi network
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);
     
@@ -50,11 +50,9 @@ void setup() {
     }
     Serial.println("");
     
-    // This is the IP address you need to copy into MIT App Inventor
     Serial.print("ESP32-S2 IP Address: ");
     Serial.println(WiFi.localIP()); 
 
-    // Define the web endpoints the iPhone will check
     server.on("/status", handleStatus);
     server.on("/reset", handleReset);
     server.begin();
@@ -62,44 +60,34 @@ void setup() {
 
 void loop() {
     server.handleClient();
-    
-    // HIGH means the button is NOT pressed (idle state)
+
     if (digitalRead(triggerPin) == HIGH) {
-        
-        // Only change the status if we haven't already sent a play command
-        if (!soundTriggered) {
-            
-            if (counter < 100) {
-                currentStatus = "PLAY1";
-            } else if (counter < 200) {
-                currentStatus = "PLAY2";
-            } else if (counter < 300) {
-                currentStatus = "PLAY3";
-            } else if (counter < 400) {
-                currentStatus = "PLAY4";
-            } else if (counter < 500) {
-                currentStatus = "PLAY5";
-            } else {
-                currentStatus = "PLAY6";
-            }
-            
-            Serial.print("Button is NOT pressed! Status set to: ");
-            Serial.println(currentStatus);
-            
-            // Lock it down so it doesn't spam. 
-            // It stays locked until the app calls /reset
-            soundTriggered = true; 
-        }
+        if (counter < 100)       currentStatus = "PLAY1";
+        else if (counter < 200)  currentStatus = "PLAY2";
+        else if (counter < 300)  currentStatus = "PLAY3";
+        else if (counter < 400)  currentStatus = "PLAY4";
+        else if (counter < 500)  currentStatus = "PLAY5";
+        else if (counter < 550)  currentStatus = "PLAY6";
+
+        counter++;
     } else {
-        // Optional: If they DO press the button, reset the tracker 
-        // or change the status if you want a "pressed" behavior.
+        currentStatus = "PLAYPURR";
+        counter = 0;
     }
 
-    // Cycle the counter in the background to mix up the sounds
-    counter++;
-    if (counter >= 600) {
-        counter = 0; 
-    }
-    
-    delay(50); 
+    // Trigger servo sweep when counter maxes out
+if (counter >= 0 && !thrown) {
+    myServo.write(180);  // spin one direction
+    delay(500);          // tune this ms value until it rotates ~70°
+    myServo.write(90);   // stop
+
+    myServo.write(0);  // spin one direction
+    delay(500);          // tune this ms value until it rotates ~70°
+    myServo.write(90);   // stop
+    thrown = true;
+}
+
+    Serial.println("Current status: " + currentStatus);
+
+    delay(50);
 }
